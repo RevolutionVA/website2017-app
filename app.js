21bd7b6e8b3e9473088c91880f14881a1a46c04f
Brian Reed <me@iambrian.com>
2017-02-02 01:08:55 -0500

    removed ssl in preparation for heroku

--- app.js
+++ app.js
@@ -5,32 +5,6 @@ const http = require('http');
 
 const HTTP_PORT = process.env.PORT || 80;
 
-if (process.env.SSL) {
-    const https = require('https');
-    const HTTPS_PORT = 443;
-    const fs = require('fs');
-    const httpsOptions = {
-        key: fs.readFileSync('/etc/letsencrypt/live/revolutionconf.com/privkey.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/revolutionconf.com/cert.pem'),
-        ca: fs.readFileSync('/etc/letsencrypt/live/revolutionconf.com/chain.pem')
-    };
-
-    app.all('*', function (req, res, next) {
-
-        if (req.secure) {
-            return next();
-        }
-
-        res.redirect('https://' + req.hostname + ':' + HTTPS_PORT + req.url);
-    });
-
-    https.createServer(httpsOptions, app)
-        .listen(HTTPS_PORT, function () {
-
-            console.log('Secure Server listening on port ' + HTTPS_PORT);
-        });
-}
-
 app.set('view engine', 'ejs');
 
 app.use('/', express.static('public'));
