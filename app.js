4a3264bd9f7b8a294cfdc6c803624e542d06844c
Brian Reed <me@iambrian.com>
2017-02-08 03:53:14 -0500

    comments

--- app.js
+++ app.js
@@ -6,18 +6,23 @@ const http = require('http');
 const HTTP_PORT = process.env.PORT || 80;
 const redirectUrls = require('./services/redirectUrls');
 
+// handlebars view
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-
 app.set('view engine', 'handlebars');
 
+// redirections
 app.use('*', redirectUrls);
 
+// build auth
 app.use('/build', require('./services/builder').use);
 
+// static stuff
 app.use('/', express.static('public'));
 
+// build getter
 app.get('/build', require('./services/builder').get);
 
+// everything else
 app.get('*', require('./services/router'));
 
 http.createServer(app).listen(HTTP_PORT, function () {
