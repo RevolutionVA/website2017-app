c26b18cbac38e3919750c1bcdec06bce9b56b4d7
Brian Reed <me@iambrian.com>
2017-02-08 04:34:14 -0500

    adding redirect logs

--- services/redirectUrls.js
+++ services/redirectUrls.js
@@ -10,16 +10,20 @@ module.exports = function (req, res, next) {
         ? req.headers.host.slice(0, req.headers.host.indexOf(':'))
         : req.headers.host;
 
+    let redirectionType = 'None';
+
     if (process.env['REDIRECT:' + hostname]) {
+        redirectionType = 'environment variable';
         redirectTo = process.env['REDIRECT:' + hostname];
     }
 
     else if (process.env.SSL_REDIRECT && 'http' === req.headers['x-forwarded-proto']) {
+        redirectionType = 'ssl redirect';
         redirectTo = 'https://' + req.headers.host + req.url;
     }
 
     if (redirectTo && redirectTo !== redirectFrom) {
-        console.log('Redirecting ' + redirectFrom + ' to ' + redirectTo);
+        console.log('Redirecting ' + redirectFrom + ' to ' + redirectTo + ' via ' + redirectionType);
         res.redirect(redirectTo);
         return false;
     }
