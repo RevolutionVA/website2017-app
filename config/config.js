e93a2009dcd631bb3235783e3fe63604ef6edac1
Linda Nichols <lynnaloo@gmail.com>
2017-02-27 18:45:46 -0500

    [fix] removed generated content from source control

--- config/config.js
+++ config/config.js
@@ -5,28 +5,28 @@ const convict = require('convict');
 // Define a schema
 var conf = convict({
   env: {
-    doc: "The applicaton environment.",
-    format: ["production", "development", "test"],
-    default: "development",
-    env: "NODE_ENV"
+    doc: 'The applicaton environment.',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV'
   },
   port: {
-    doc: "The port to bind.",
-    format: "port",
+    doc: 'The port to bind.',
+    format: 'port',
     default: 8080,
-    env: "PORT"
+    env: 'PORT'
   },
   contentZipUrl: {
-    doc: "The content zip file url.",
+    doc: 'The content zip file url.',
     format: 'url',
     default: 'http://github.com/revolutionva/website2017/archive/master.zip',
-    env: "CONTENT_ZIP_URL"
+    env: 'CONTENT_ZIP_URL'
   },
   contentLocal: {
-    doc: "The local content",
+    doc: 'directory path for the local copy of the content repo',
     format: String,
     default: '',
-    env: "CONTENT_LOCAL"
+    env: 'CONTENT_LOCAL'
   }
 });
 
