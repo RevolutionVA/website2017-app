6866d0d618ce91f433f3b64179f94c488a491786
Linda Nichols <lynnaloo@gmail.com>
2017-02-27 18:39:46 -0500

    [config] added default environment variables to quiet local error

---
+++ config/config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const convict = require('convict');
+
+// Define a schema
+var conf = convict({
+  env: {
+    doc: "The applicaton environment.",
+    format: ["production", "development", "test"],
+    default: "development",
+    env: "NODE_ENV"
+  },
+  port: {
+    doc: "The port to bind.",
+    format: "port",
+    default: 8080,
+    env: "PORT"
+  },
+  contentZipUrl: {
+    doc: "The content zip file url.",
+    format: 'url',
+    default: 'http://github.com/revolutionva/website2017/archive/master.zip',
+    env: "CONTENT_ZIP_URL"
+  },
+  contentLocal: {
+    doc: "The local content",
+    format: String,
+    default: '',
+    env: "CONTENT_LOCAL"
+  }
+});
+
+// Perform validation
+conf.validate({strict: true});
+
+module.exports = conf;
