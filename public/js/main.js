1d51399e6143bb881d6a80a9e3863d21a546f397
Brian Reed <me@iambrian.com>
2017-05-07 12:51:59 -0400

    babeled

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
+'use strict';
+
 /* globals jQuery */
 
 jQuery(function ($) {
-    
+
     $(document.getElementById('site-title-fit-text')).fitText(0.55, {
         minFontSize: '100px'
     });
@@ -11,15 +13,15 @@ jQuery(function ($) {
         maxFontSize: '50px'
     });
 
-    $('.site-info.location').fitText(1.8, {minFontSize: '20px'});
+    $('.site-info.location').fitText(1.8, { minFontSize: '20px' });
 
-    $('.big-title').fitText(0.7, {minFontSize: '20px', maxFontSize: '50px'});
+    $('.big-title').fitText(0.7, { minFontSize: '20px', maxFontSize: '50px' });
 
     $(window).load(function () {
         $(document.body).css('opacity', 1);
     });
 
-    const sideNavToggle = $(document.getElementById('side-nav-toggle')),
+    var sideNavToggle = $(document.getElementById('side-nav-toggle')),
         sideNav = $(document.getElementById('side-nav'));
 
     sideNavToggle.on('click', function (event) {
@@ -33,45 +35,42 @@ jQuery(function ($) {
         sideNav.removeClass('open');
     });
 
-    let sliderImages = $('.section-location .images'), sliderImageWidth = 0;
+    var sliderImages = $('.section-location .images'),
+        sliderImageWidth = 0;
 
     function rotateSlider() {
         sliderImages.stop().animate({
             'margin-left': '-' + sliderImageWidth + 'px'
         }, 50000, 'linear', function () {
-            sliderImages.css({'margin-left': 0});
+            sliderImages.css({ 'margin-left': 0 });
             rotateSlider();
         });
     }
 
-    $('.slider .images img')
-        .on('load', function () {
-            sliderImageWidth = $('img', sliderImages).width();
-            rotateSlider();
-        });
+    $('.slider .images img').on('load', function () {
+        sliderImageWidth = $('img', sliderImages).width();
+        rotateSlider();
+    });
 
     $('img[data-original]').lazyload();
 
-    $('img[data-original][data-delay]')
-        .each(function () {
+    $('img[data-original][data-delay]').each(function () {
 
-            let img = $(this), delay = parseInt(img.data('delay'));
+        var img = $(this),
+            delay = parseInt(img.data('delay'));
 
-            $(window)
-                .on('load', function () {
-                    setTimeout(function () {
-                        img.trigger('delay');
-                    }, delay);
-                });
-        })
-        .lazyload({event: 'delay'});
+        $(window).on('load', function () {
+            setTimeout(function () {
+                img.trigger('delay');
+            }, delay);
+        });
+    }).lazyload({ event: 'delay' });
 
     $('a.talk-link').on('click', function (event) {
 
         event.preventDefault();
 
         $(this).closest('.event').find('.summary').toggle();
-
     });
 
     $(window).load(function () {
@@ -80,29 +79,28 @@ jQuery(function ($) {
 
     function ScheduleApp(element) {
 
-        if (!element.length)
-            return;
+        if (!element.length) return;
 
-        const saving_changes = $('.saving-changes').hide();
+        var saving_changes = $('.saving-changes').hide();
 
         $('[data-heart]', element).on('click', function () {
 
-            const heart = $(this).toggleClass('unchecked');
+            var heart = $(this).toggleClass('unchecked');
             favorite(heart.data('heart'), !heart.hasClass('unchecked'));
         });
 
-        let favorites = window.localStorage.getItem('revConfSchedule').split(',') || [],
+        var favorites = window.localStorage.getItem('revConfSchedule').split(',') || [],
             saveTimeout = false;
 
         favorites = Array.isArray(favorites) ? favorites : [];
 
-        $.each(favorites, (index, favorite) => {
+        $.each(favorites, function (index, favorite) {
             $('[data-heart="' + favorite + '"]').removeClass('unchecked');
         });
 
         function favorite(eventSlug, set) {
 
-            const index = favorites.indexOf(eventSlug);
+            var index = favorites.indexOf(eventSlug);
 
             if (set) {
                 index === -1 && favorites.push(eventSlug) && updateStorage();
@@ -113,8 +111,7 @@ jQuery(function ($) {
 
         function updateStorage() {
 
-            if (saveTimeout)
-                clearTimeout(saveTimeout);
+            if (saveTimeout) clearTimeout(saveTimeout);
 
             saving_changes.show();
 
@@ -124,6 +121,4 @@ jQuery(function ($) {
             }, 1000);
         };
     }
-
-
 });
